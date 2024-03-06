d2330bf0f7f1675db985db11b5f051ae872f50ba
Sesha-Sai1 <seshasaikothapalli1350@gmail.com>
2024-03-06 17:55:55 +0530

    responsiveness

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -8,14 +8,18 @@ const MainPage = () => {
     <div className="container-fluid">
       <div
         className="row "
-        style={{ backgroundImage: "url(./Images/bg1.jpg)", height: "100vh" }}
+        style={{
+          backgroundImage: "url(./Images/bg1.jpg)",
+          height: "100vh",
+        }}
       >
         <div className="col-sm-12 col-lg-8 col-md-6 align-middle my-5 text-center">
-          <img src="./Images/tomato.png" className="w-75 h-75" />
+          <img src="./Images/tomato.png" className="w-50 h-75" />
         </div>
         <div
-          className="col-sm-12 col-lg-4 col-md-6 "
-          style={{ marginTop: "250px" }}
+          className="col-sm-12 col-lg-4 col-md-6"
+          id="matter"
+          // style={{ marginTop: "230px" }}
         >
           <h1>POMODORO TIMER</h1>
           <p>
